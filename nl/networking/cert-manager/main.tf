0dca683af41bde3ace53c2a758d5b238bc301b5e
Danilo Cianfrone <danilocianfr@gmail.com>
2025-01-17 22:19:01 +0100

    feat(nl/networking): add cert-manager

---
+++ nl/networking/cert-manager/main.tf
@@ -0,0 +1,68 @@
+resource "helm_release" "cert_manager" {
+  name             = "cert-manager"
+  repository       = "https://charts.jetstack.io"
+  chart            = "cert-manager"
+  namespace        = "networking"
+  version          = "1.16.3"
+  create_namespace = true
+  cleanup_on_fail  = true
+
+  values = [yamlencode({
+    crds = { enabled = true }
+    prometheus = {
+      enabled        = true
+      servicemonitor = { enabled = true }
+    }
+  })]
+}
+
+variable "cloudflare_api_token" {
+  type        = string
+  description = "Cloudflare API token to use for DNS01 challenges"
+  sensitive   = true
+}
+
+resource "kubernetes_secret" "cert_manager_cloudflare_api_token" {
+  metadata {
+    name      = "cloudflare-api-token"
+    namespace = "networking"
+  }
+
+  data = {
+    api-token = var.cloudflare_api_token
+  }
+}
+
+
+resource "kubernetes_manifest" "cert_manager_acme_issuer" {
+  depends_on = [helm_release.cert_manager]
+
+  manifest = {
+    apiVersion = "cert-manager.io/v1"
+    kind       = "ClusterIssuer"
+
+    metadata = {
+      name = "acme"
+    }
+
+    spec = {
+      acme = {
+        email  = "danilocianfr+letsencrypt@gmail.com"
+        server = "https://acme-v02.api.letsencrypt.org/directory"
+        privateKeySecretRef = {
+          name = "cert-manager-acme-issuer-account-key"
+        }
+        solvers = [{
+          dns01 = {
+            cloudflare = {
+              apiTokenSecretRef = {
+                name = kubernetes_secret.cert_manager_cloudflare_api_token.metadata[0].name
+                key  = "api-token"
+              }
+            }
+          }
+        }]
+      }
+    }
+  }
+}
