773aff92c12b1df60e537ab9010c9b329eb1a99c
Danilo Cianfrone <danilocianfr@gmail.com>
2025-03-18 21:04:59 +0100

    feat(nl/media): add lidarr

---
+++ clusters/nl/media/lidarr/main.tf
@@ -0,0 +1,203 @@
+resource "kubernetes_persistent_volume_v1" "lidarr_media" {
+  metadata {
+    name = "lidarr-media-pv"
+  }
+
+  spec {
+    storage_class_name = "local-path"
+    access_modes       = ["ReadWriteOnce"]
+
+    capacity = {
+      storage = "263.7G" # NOTE: this is the size of the partition.
+    }
+
+    persistent_volume_source {
+      host_path {
+        path = "/media"
+      }
+    }
+
+    node_affinity {
+      required {
+        node_selector_term {
+          match_expressions {
+            key      = "kubernetes.io/hostname"
+            operator = "In"
+            values   = ["eq14-001"]
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_persistent_volume_claim_v1" "lidarr_media" {
+  metadata {
+    name      = "lidarr-media-pvc"
+    namespace = "media"
+  }
+
+  spec {
+    storage_class_name = "local-path"
+    access_modes       = ["ReadWriteOnce"]
+    volume_name        = kubernetes_persistent_volume_v1.lidarr_media.metadata[0].name
+
+    resources {
+      requests = {
+        storage = "263.7G" # NOTE: this is the size of the partition.
+      }
+    }
+  }
+}
+
+resource "kubernetes_persistent_volume_v1" "lidarr_config" {
+  metadata {
+    name = "lidarr-config-pv"
+  }
+
+  spec {
+    storage_class_name = "local-path"
+    access_modes       = ["ReadWriteOnce"]
+
+    capacity = {
+      storage = "100M" # NOTE: this is the size of the partition.
+    }
+
+    persistent_volume_source {
+      host_path {
+        path = "/media/config/lidarr"
+      }
+    }
+
+    node_affinity {
+      required {
+        node_selector_term {
+          match_expressions {
+            key      = "kubernetes.io/hostname"
+            operator = "In"
+            values   = ["eq14-001"]
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_persistent_volume_claim_v1" "lidarr_config" {
+  metadata {
+    name      = "lidarr-config-pvc"
+    namespace = "media"
+  }
+
+  spec {
+    storage_class_name = "local-path"
+    access_modes       = ["ReadWriteOnce"]
+    volume_name        = kubernetes_persistent_volume_v1.lidarr_config.metadata[0].name
+
+    resources {
+      requests = {
+        storage = "100M"
+      }
+    }
+  }
+}
+
+resource "helm_release" "lidarr" {
+  name             = "lidarr"
+  repository       = "https://bjw-s.github.io/helm-charts"
+  chart            = "app-template"
+  namespace        = "media"
+  version          = "3.7.3"
+  create_namespace = true
+  cleanup_on_fail  = true
+
+  values = [yamlencode({
+    defaultPodOptions = {
+      securityContext = {
+        runAsUser           = 1000
+        runAsGroup          = 1000
+        fsGroup             = 1000
+        fsGroupChangePolicy = "OnRootMismatch"
+      }
+    }
+
+    controllers = {
+      main = {
+        type     = "statefulset"
+        replicas = 1
+
+        annotations = {
+          "reloader.stakater.com/auto" = "true"
+        }
+
+        containers = {
+          main = {
+            image = {
+              repository = "ghcr.io/linuxserver/lidarr"
+              tag        = "2.9.6"
+            }
+            env = {
+              TZ    = "Europe/Amsterdam"
+              PGUID = 1000
+              PGID  = 1000
+            }
+            probes = {
+              # FIXME(ar3s3ru): find a way to enable these?
+              liveness  = { enabled = false }
+              readiness = { enabled = false }
+              startup   = { enabled = true }
+            }
+          }
+        }
+      }
+    }
+
+    service = {
+      main = {
+        controller = "main"
+        type       = "ClusterIP"
+        ports = {
+          http = {
+            port = 8686
+          }
+        }
+      }
+    }
+
+    ingress = {
+      tailscale = {
+        enabled   = true
+        className = "tailscale"
+
+        hosts = [{
+          host = "nl-lidarr",
+          paths = [{
+            path     = "/",
+            pathType = "Prefix",
+            service = {
+              identifier = "main",
+              port       = "http"
+            }
+          }]
+        }]
+
+        tls = [{ hosts = ["nl-lidarr"] }]
+      }
+    }
+
+    persistence = {
+      config = {
+        enabled       = true
+        type          = "persistentVolumeClaim"
+        existingClaim = kubernetes_persistent_volume_claim_v1.lidarr_config.metadata[0].name
+        globalMounts  = [{ path = "/config" }]
+      }
+      media = {
+        enabled       = true
+        type          = "persistentVolumeClaim"
+        existingClaim = kubernetes_persistent_volume_claim_v1.lidarr_media.metadata[0].name
+        globalMounts  = [{ path = "/media" }]
+      }
+    }
+  })]
+}
