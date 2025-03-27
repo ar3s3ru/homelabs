d2019e90ee0e87d2154d92db93d5e823f44d0836
Danilo Cianfrone <6481571+ar3s3ru@users.noreply.github.com>
2025-03-27 10:02:38 +0100

    fix(nl): use tailscale unstable, music-assistant privileged container (#24)
    * fix(modules/tailscale): move to v1.81.110 to fix the open file limit issue
    
    * fix(nl/music-assistant): use privileged container to fix startup issue

--- modules/tailscale/main.tf
+++ modules/tailscale/main.tf
@@ -34,9 +34,10 @@ resource "kubernetes_cluster_role_binding" "ar3s3ru_cluster_admin" {
 }
 
 resource "helm_release" "tailscale_operator" {
-  name             = "tailscale-operator"
-  repository       = "https://pkgs.tailscale.com/helmcharts"
-  version          = "1.80.3"
+  name = "tailscale-operator"
+  # NOTE: switched to the "unstable" Helm chart to fix https://github.com/tailscale/tailscale/issues/15081
+  repository       = "https://pkgs.tailscale.com/unstable/helmcharts"
+  version          = "1.81.110"
   chart            = "tailscale-operator"
   namespace        = var.kubernetes_namespace
   create_namespace = true
