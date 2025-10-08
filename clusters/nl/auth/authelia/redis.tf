be25964c7c60cc07df3b7714dd3958c46cc31d93
Danilo Cianfrone <danilocianfr@gmail.com>
2025-10-08 19:10:31 +0200

    feat: move authelia to redis-operator instance, fix ingress-controller ip

---
+++ clusters/nl/auth/authelia/redis.tf
@@ -0,0 +1,13 @@
+resource "helm_release" "redis" {
+  name             = "authelia-redis"
+  repository       = "https://ot-container-kit.github.io/helm-charts/"
+  chart            = "redis"
+  version          = "0.16.6"
+  namespace        = local.namespace
+  create_namespace = false
+  cleanup_on_fail  = true
+
+  values = [
+    file("${path.module}/values-redis.yaml")
+  ]
+}
