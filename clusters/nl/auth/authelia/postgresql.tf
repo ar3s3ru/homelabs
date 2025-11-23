461e8218cdd2d603b78809aea884aa5d1998a1bb
Danilo Cianfrone <danilocianfr@gmail.com>
2025-11-24 00:45:10 +0100

    feat: remove clusters/nl/auth, now on argocd

--- clusters/nl/auth/authelia/postgresql.tf
+++
@@ -1,3 +0,0 @@
-resource "kubernetes_manifest" "cnpg_cluster" {
-  manifest = yamldecode(file("${path.module}/values-cnpg.yaml"))
-}
--- clusters/nl/auth/namespace.tf
+++
@@ -1,5 +0,0 @@
-resource "kubernetes_namespace" "auth" {
-  metadata {
-    name = "auth"
-  }
-}
