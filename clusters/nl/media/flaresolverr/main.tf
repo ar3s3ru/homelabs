8fc58412958c7b47de536f6ac06ac193f65f33e0
Danilo Cianfrone <danilocianfr@gmail.com>
2025-11-23 23:34:29 +0100

    refactor: remove clusters/nl/media, migrate to kube

--- clusters/nl/media/flaresolverr/main.tf
+++
@@ -1,9 +0,0 @@
-resource "helm_release" "flaresolverr" {
-  name            = "flaresolverr"
-  repository      = "https://bjw-s-labs.github.io/helm-charts"
-  chart           = "app-template"
-  namespace       = "media"
-  version         = "4.4.0"
-  cleanup_on_fail = true
-  values          = [file("./values.yaml")]
-}
--- clusters/nl/media/namespace.tf
+++
@@ -1,5 +0,0 @@
-resource "kubernetes_namespace" "media" {
-  metadata {
-    name = "media"
-  }
-}
