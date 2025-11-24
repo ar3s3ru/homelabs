57d5160e62496da644c31aa91a21b3560ce80551
Danilo Cianfrone <danilocianfr@gmail.com>
2025-11-24 13:14:41 +0100

    feat: clean up terragrunt

--- clusters/nl/home-automation/namespace.tf
+++
@@ -1,5 +0,0 @@
-resource "kubernetes_namespace" "home-automation" {
-  metadata {
-    name = "home-automation"
-  }
-}
--- clusters/nl/productivity/namespace.tf
+++
@@ -1,5 +0,0 @@
-resource "kubernetes_namespace" "productivity" {
-  metadata {
-    name = "productivity"
-  }
-}
