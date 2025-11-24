cfb44930fef36ed3dbe392b57ea647cc879f4f37
Danilo Cianfrone <danilocianfr@gmail.com>
2025-11-24 11:32:02 +0100

    feat(kube): migrate kyverno-system, clean up clusters/nl

--- clusters/nl/kyverno-system/main.tf
+++
@@ -1,9 +0,0 @@
-resource "helm_release" "kyverno" {
-  name             = "kyverno"
-  repository       = "https://kyverno.github.io/kyverno"
-  chart            = "kyverno"
-  namespace        = "kyverno-system"
-  version          = "3.5.2"
-  create_namespace = true
-  cleanup_on_fail  = true
-}
