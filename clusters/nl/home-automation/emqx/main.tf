9913ac5b73f31bae2c4cb57a3a00ef69d19a836d
Danilo Cianfrone <danilocianfr@gmail.com>
2025-11-24 12:16:59 +0100

    feat(kube/home-automation): migrate emqx

--- clusters/nl/home-automation/emqx/main.tf
+++
@@ -1,9 +0,0 @@
-resource "helm_release" "emqx" {
-  name            = "emqx"
-  repository      = "https://repos.emqx.io/charts"
-  chart           = "emqx"
-  namespace       = "home-automation"
-  version         = "5.8.8"
-  cleanup_on_fail = true
-  values          = [file("./values.yaml")]
-}
